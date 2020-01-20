8b5b4bcce5e91fd18584f16f36d56b28cb6a416e
kh3996 <kh3996@gmail.com>
2020-01-20 13:54:09 -0400

    dependency injection with out using NgModule

--- dependency-injection/src/app/app.module.ts
+++ dependency-injection/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserDemoComponent } from './user-demo/user-demo.component';
+import { UserDemoInjectorComponent } from './user-demo/user-demo.injector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDemoComponent
+    UserDemoComponent,
+    UserDemoInjectorComponent
   ],
   imports: [
     BrowserModule,
