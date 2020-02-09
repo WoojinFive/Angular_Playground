e9ed3e413bdd7fd1741968bf90d31f6857e7f2fd
kh3996 <kh3996@gmail.com>
2020-02-08 23:33:13 -0400

    material-demo

---
+++ material-demo/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  isChecked = true;
+  colors = [
+    {
+      id: 1,
+      name: 'Red'
+    },
+    {
+      id: 2,
+      name: 'Green'
+    },
+    {
+      id: 3,
+      name: 'Blue'
+    }
+  ];
+
+  color = 2;
+
+  onChange($event) {
+    console.log($event);
+  }
+}
---
+++ material-demo/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Material
+import { MaterialModule } from './material/material.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    // Material
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
