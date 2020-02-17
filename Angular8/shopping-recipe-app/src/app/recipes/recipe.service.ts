1dd335f4687ab22389e725e834493982790aa5e1
kh3996 <kh3996@gmail.com>
2020-02-17 00:51:37 -0400

    keep going shop list app

--- Angular8/shopping-recipe-app/src/app/recipes/recipe.service.ts
+++ Angular8/shopping-recipe-app/src/app/recipes/recipe.service.ts
@@ -1,12 +1,13 @@
-import { Recipe } from './recipe.model';
 import { EventEmitter, Injectable } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+
 import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
 
 @Injectable()
 
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
+  // recipeSelected = new EventEmitter<Recipe>();
   
   private recipes: Recipe[] = [
     new Recipe(
